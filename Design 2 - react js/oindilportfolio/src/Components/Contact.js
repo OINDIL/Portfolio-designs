43cd7dec2dcd7080cc02a61dcbac04f294d31099
oindil <oindilgolder303@gmail.com>
2023-12-28 19:41:45 +0530

    added contact pafe

--- Design 2 - react js/oindilportfolio/src/Components/Contact.js
+++ Design 2 - react js/oindilportfolio/src/Components/Contact.js
@@ -1,8 +1,22 @@
 export default function Contact(){
     return (
         <>
-            <div className="contact-container">
-                
+            <div className="contact">
+                <div className="contact-container">
+                    <div className="contact-label">
+                        <p>Contact</p>
+                    </div>
+                    <div className="contact-form">
+                        <form>
+                            <label htmlFor="name">Name</label>
+                            <input type="text" id="name" />
+                            <label htmlFor="address">E-mail</label>
+                            <input type="email" id="address"/>
+                            <label htmlFor="message">Message</label>
+                            <textarea name="message" id="message" cols="30"></textarea>
+                        </form>
+                    </div>
+                </div>
             </div>
         </>
     )
--- Design 2 - react js/oindilportfolio/src/Components/Projects.js
+++ Design 2 - react js/oindilportfolio/src/Components/Projects.js
@@ -1,11 +1,29 @@
-export default function Project(){
-    return (
-        <>
-            <div className="projects-container">
-                <div className="projects-image">
-                    <p>Password Generator - thumbnail</p>
-                </div>
-            </div>
-        </>
-    )
+export default function Project() {
+  return (
+    <>
+    <div className="projects">
+        <p>Projects</p>
+      <div className="projects-container">
+        <div className="project-1">
+          <div className="thumbnail">
+            <p></p>
+          </div>
+          <div className="desc">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione, quis! Iste veniam esse quidem sapiente assumenda perspiciatis aliquam est culpa pariatur, hic reiciendis recusandae soluta qui deserunt amet quo. Ipsum at officia voluptates voluptatibus rerum sit illum laboriosam veritatis sunt!</p>
+          </div>
+        </div>
+
+
+        <div className="project-2">
+          <div className="thumbnail">
+            <p></p>
+          </div>
+          <div className="desc">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione, quis! Iste veniam esse quidem sapiente assumenda perspiciatis aliquam est culpa pariatur, hic reiciendis recusandae soluta qui deserunt amet quo. Ipsum at officia voluptates voluptatibus rerum sit illum laboriosam veritatis sunt!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
+  );
 }
