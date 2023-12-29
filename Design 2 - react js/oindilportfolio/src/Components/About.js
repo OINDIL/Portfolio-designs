8fce3c0a85886e6a2d15a335ea5f1b73d1c92311
oindil <oindilgolder303@gmail.com>
2023-12-29 23:03:56 +0530

    added about part

--- Design 2 - react js/oindilportfolio/src/Components/About.js
+++ Design 2 - react js/oindilportfolio/src/Components/About.js
@@ -1,34 +1,40 @@
-export default function About(){
-    return (
-        <>
-            <div className="about" id="about">
-                <p>About</p>
-                <div className="about-container">
-                    <div className="about-image-skills">
-                        <p>TECH STACK</p>
-                    </div>
-                    <div className="about-skills">
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam iste facere possimus sequi quis cumque dolores asperiores aliquid. Praesentium inventore consectetur, aliquid voluptate dolore expedita dolorum eveniet velit blanditiis quisquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae repellendus ullam corporis nesciunt iste beatae ipsam accusantium veniam tempora, nulla itaque laboriosam, impedit incidunt doloribus a. Dolorem, saepe similique tempore, est dicta, molestiae animi voluptatem sequi laborum assumenda suscipit expedita.</p>
-                        <div className="skills-icons">
-                        <i className='bx bxl-html5'></i>
-                        <i className='bx bxl-css3' ></i>
-                        <i className='bx bxl-javascript' ></i>
-                        <i className='bx bxl-react' ></i>
-                        <i className='bx bxl-mongodb'></i>
-                        <i className='bx bxl-java'></i>
-                        <i className='bx bxl-nodejs' ></i>
-                        </div>
-                    </div>
-                </div>
-                <div className="hobby-container">
-                    <div className="about-image-hobby">
-                        <p>PHOTOGRAPHY📸</p>
-                    </div>
-                    <div className="about-hobby">
-                        <p>images will go here, overflow-y todo</p>
-                    </div>
-                </div>
+export default function About() {
+  return (
+    <>
+      <div className="about" id="about">
+        <p>
+          <span>About</span>
+        </p>
+        <div className="about-container">
+          <div className="about-image-skills">
+            <p>TECH STACK</p>
+          </div>
+          <div className="about-skills">
+            <h3>Technologies I have experience in</h3>
+            <p>
+              I have great experience in languages like html, css , javascript , c , java and javascript library such as <span id="react-animation">"react"</span>.I also have familarities with version control system like github. I have built many project on the web. Those projects can be found in the <span id="project-animation-link"><a href="#projects">projects</a></span> section.
+            </p>
+            <div className="skills-icons">
+              <i className="bx bxl-html5"></i>
+              <i className="bx bxl-css3"></i>
+              <i className="bx bxl-javascript"></i>
+              <i className="bx bxl-react"></i>
+              <i className="bx bxl-mongodb"></i>
+              <i className="bx bxl-java"></i>
+              <i className="bx bxl-nodejs"></i>
+              <i class='bx bxl-git'></i>
             </div>
-        </>
-    )
+          </div>
+        </div>
+        <div className="hobby-container">
+          <div className="about-image-hobby">
+            <p>PHOTOGRAPHY📸</p>
+          </div>
+          <div className="about-hobby">
+            <p>images will go here, overflow-y todo</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
